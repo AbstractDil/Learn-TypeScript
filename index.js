a33fba3d502fca49bbd4ece6bf18a3ef93e8f096
AbstractDil <nandysagar@yahoo.com>
2023-07-20 22:11:56 +0530

    first typescript code

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("Hello TypeScript!");
+var num1 = 15;
+// num1 = "Hello"; // Error: Type 'string' is not assignable to type 'number'.
+var num2 = 20;
+console.log(num1 + num2);
---
+++ index.ts
@@ -0,0 +1,10 @@
+console.log("Hello TypeScript!");
+
+let num1:number = 15;
+
+// num1 = "Hello"; // Error: Type 'string' is not assignable to type 'number'.
+
+let num2: number = 20;
+
+console.log(num1 + num2);
+
