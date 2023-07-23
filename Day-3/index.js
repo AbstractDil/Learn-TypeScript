39826a6407f000d53bc37bcef24e2ebd1b475141
AbstractDil <nandysagar@yahoo.com>
2023-07-23 14:36:09 +0530

    Type Annotation Number and String

---
+++ Day-3/index.js
@@ -0,0 +1,70 @@
+"use strict";
+/*
+TYpe Annotations
+
+@Author: Sagar Nandy
+@Date: 23/07/2023
+*/
+/*
+* Number Type
+*/
+let apples = 5; // isValid = true
+let speed = 10.5; // isValid = true
+let hasName = true; // isValid = true
+let nothingMuch = null; // isValid = true
+let nothing = undefined; // isValid = true
+//let myFavNum:number= 7 + "Sagar"; // isValid = false because myFavNum is number type 
+// console.log(myFavNum); // Output: 7Sagar but typescript will show error
+let totalApples = apples + 5; // isValid = true because totalApples is number type
+console.log(totalApples);
+/*
+* String Type
+*/
+let myName = "Sagar"; // isValid = true
+let sentence = `My name is ${myName}`; // isValid = true
+console.log(sentence); // Output: My name is Sagar
+let myFirstName = "Sagar";
+let myLastName = "Nandy";
+let myFullName = myFirstName + myLastName; // isValid = true
+console.log(myFullName); // Output: SagarNandy
+/*
+* HomeWork
+*/
+// String Initialization
+let message = "Hello TypeScript"; // isValid = true
+// Concatenation
+let myMessage = message + " " + "Welcome to TypeScript"; // isValid = true
+console.log(myMessage); // Output: Hello TypeScript Welcome to TypeScript
+// string length
+let messageLength = message.length; // isValid = true
+console.log(messageLength); // Output: 17
+console.log(myMessage.length); // Output: 32
+// string to uppercase
+let upperCaseMessage = message.toUpperCase(); // isValid = true
+console.log(upperCaseMessage); // Output: HELLO TYPESCRIPT
+// string to lowercase
+let lowerCaseMessage = message.toLowerCase(); // isValid = true
+console.log(lowerCaseMessage); // Output: hello typescript
+//Substring
+let subStringMessage = message.substring(0, 5); // isValid = true
+console.log(subStringMessage); // Output: Hello
+// string comparison
+let firstMessage = "Hello";
+let secondMessage = "Hello";
+let thirdMessage = "hello";
+let fourthMessage = "Hello TypeScript";
+let fifthMessage = "Hello TypeScript";
+let sixthMessage = "Hello TypeScript";
+console.log(firstMessage == secondMessage); // Output: true
+console.log(firstMessage == thirdMessage); // Output: false
+console.log(fourthMessage == fifthMessage); // Output: true
+console.log(fourthMessage == sixthMessage); // Output: true
+console.log(fifthMessage == sixthMessage); // Output: true
+console.log(firstMessage == fourthMessage); // Output: false
+console.log(firstMessage == fifthMessage); // Output: false
+console.log(firstMessage == sixthMessage); // Output: false
+console.log(secondMessage == thirdMessage); // Output: false
+console.log(secondMessage == fourthMessage); // Output: false
+// string Template
+let myMessageTemplate = `Hello TypeScript Welcome to TypeScript`; // isValid = true
+console.log(myMessageTemplate); // Output: Hello TypeScript Welcome to TypeScript
--- Readme.md
+++ Readme.md
@@ -2,3 +2,20 @@
 # Typescript
 
 TypeScript is JavaScript with syntax for types.
+
+# Commands
+
+```bash 
+tsc --init # Create tsconfig.json
+tsc # Compile all .ts files
+tsc --watch # Compile all .ts files and watch for changes
+```
+# Use of noEmitOnError
+
+```json
+{
+  "compilerOptions": {
+    "noEmitOnError": true
+  }
+}
+```
